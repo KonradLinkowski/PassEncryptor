30cdf84d14b9106cfec926cdf6d0a905c4283c79
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-04-06 12:13:13 +0200

    returning hash

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const bcrypt = require('bcryptjs')
 const prompt = require('prompt')
 
-
 let promptSchema = {
   properties: {
     password: {
@@ -10,12 +9,37 @@ let promptSchema = {
       required: true,
       hidden: true,
       replace: '*',
+      type: 'string'
+    },
+    salt_factor: {
+      name: 'salt_factor',
+      message: 'Enter the salt factor',
+      required: false,
+      default: 10,
+      type: 'integer'
     }
   }
 }
 
 prompt.start();
 
-prompt.get(promptSchema, function (err, result) {
-  console.log('  password: ' + result.password);
+prompt.get(promptSchema, (err, result) => {
+  hashPassword(result.password, result.salt_factor, (err, hash) => {
+    if (err) {
+      throw err
+    }
+    console.log(hash)
+  })
 });
+
+
+function hashPassword(password, salt_factor, callback) {
+  bcrypt.genSalt(salt_factor, (err, salt) => {
+    if (err) return callback(err);
+
+    bcrypt.hash(password, salt, (err, hash) => {
+        if (err) return callback(err);
+        return callback(null, hash)
+    })
+  })
+}
